d6befb532d040079987e7686f579d8869871b322
paullewisn <paul-lewis.nunn@comparethemarket.com>
2016-08-11 08:37:58 +0100

    first commit

---
+++ RomanNumerals.Tests/Tests.cs
@@ -0,0 +1,125 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace RomanNumerals.Tests
+{
+    /// <summary>
+    ///     Add roman numerals, works up to total of 39
+    /// </summary>
+    [TestFixture]
+    internal class Tests
+    {
+        private static readonly string testNumeral = "LXXXIX";
+        private static readonly int testNumeric = 89;
+
+        [Test]
+        public void Generate50Is()
+        {
+            var iString = Numerals.IGenerator(50);
+            var result = iString.Where(a => a != 'I').ToList();
+            Assert.That(result.Count == 0, Is.True);
+        }
+
+        [Test]
+        public void Generate50Letters()
+        {
+            Assert.That(Numerals.IGenerator(50).Length == 50, Is.True);
+        }
+
+        [Test]
+        public void IaddIequalsII()
+        {
+            Assert.That(Numerals.Addition("I", "I") == "II", Is.True);
+        }
+
+        [Test]
+        public void IIaddIequals()
+        {
+            Assert.That(Numerals.Addition("II", "I") == "III", Is.True);
+        }
+
+        [Test]
+        public void IIaddVequalsVII()
+        {
+            Assert.That(Numerals.Addition("II", "V") == "VII", Is.True);
+        }
+
+        [Test]
+        public void IIIaddIequalsIV()
+        {
+            Assert.That(Numerals.Addition("III", "I") == "IV", Is.True);
+        }
+
+        [Test]
+        public void IVaddIequalsV()
+        {
+            Assert.That(Numerals.Addition("IV", "I") == "V", Is.True);
+        }
+
+        [Test]
+        public void LIVaddXXXVequalsLXXXIX() /*54+35*/
+        {
+            Assert.That(Numerals.Addition("LIV", "XXXV") == "LXXXIX", Is.True);
+        }
+
+        [Test]
+        public void LXXXIXaddXIequalsC() /*89+11*/
+        {
+            Assert.That(Numerals.Addition("LXXXIX", "XI") == "C", Is.True);
+        }
+
+        [Test]
+        public void TranslateItoNumeral()
+        {
+            var translateOutput = Numerals.IGenerator(testNumeric);
+            foreach (var translation in Numerals.TranslateFromI())
+            {
+                translateOutput = translateOutput.Replace(translation.Key, translation.Value);
+            }
+            Assert.That(translateOutput == testNumeral, Is.True);
+        }
+
+        [Test]
+        public void TranslateNumeralToI()
+        {
+            var result = Numerals.TranslateInput(testNumeral);
+            Assert.That(result == Numerals.IGenerator(testNumeric), result.Length.ToString());
+        }
+
+        [Test]
+        public void VaddIIequalsVII()
+        {
+            Assert.That(Numerals.Addition("V", "II") == "VII", Is.True);
+        }
+
+        [Test]
+        public void VIIIaddIequalsIX()
+        {
+            Assert.That(Numerals.Addition("VIII", "I") == "IX", Is.True);
+        }
+
+        [Test]
+        public void XIXaddXequalsXXIX()
+        {
+            Assert.That(Numerals.Addition("XIX", "X") == "XXIX", Is.True);
+        }
+
+        [Test]
+        public void XXXIIIaddVIequalsXXXIX() /*33+6*/
+        {
+            Assert.That(Numerals.Addition("XXXIII", "VI") == "XXXIX", Is.True);
+        }
+
+        [Test]
+        public void XXXIIIaddVIIequalsXL() /*33+7*/
+        {
+            Assert.That(Numerals.Addition("XXXIII", "VII") == "XL", Is.True);
+        }
+
+        [Test]
+        public void XXXIIIaddVIIIequalsXLI() /*33+8*/
+        {
+            Assert.That(Numerals.Addition("XXXIII", "VIII") == "XLI", Is.True);
+        }
+    }
+}
---
+++ RomanNumerals/Numerals.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace RomanNumerals
+{
+    public static class Numerals
+    {
+        public static List<KeyValuePair<string, string>> TranslateToI()
+        {
+            var translate = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("IV", IGenerator(4)),
+                new KeyValuePair<string, string>("V", IGenerator(5)),
+                new KeyValuePair<string, string>("XL", IGenerator(40)),
+                new KeyValuePair<string, string>("L", IGenerator(50)),
+                new KeyValuePair<string, string>("XC", IGenerator(90)),
+                new KeyValuePair<string, string>("C", IGenerator(100)),
+                new KeyValuePair<string, string>("IX", IGenerator(9)),
+                new KeyValuePair<string, string>("X", IGenerator(10))
+            };
+            return translate;
+        }
+
+
+        public static List<KeyValuePair<string, string>> TranslateFromI()
+        {
+            var translate = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>(IGenerator(100), "C"),
+                new KeyValuePair<string, string>(IGenerator(90), "XC"),
+                new KeyValuePair<string, string>(IGenerator(50), "L"),
+                new KeyValuePair<string, string>(IGenerator(40), "XL"),
+                new KeyValuePair<string, string>(IGenerator(10), "X"),
+                new KeyValuePair<string, string>(IGenerator(9), "IX"),
+                new KeyValuePair<string, string>(IGenerator(5), "V"),
+                new KeyValuePair<string, string>(IGenerator(4), "IV")
+            };
+            return translate;
+        }
+
+        public static string IGenerator(int count)
+        {
+            var output = string.Empty;
+            for (var i = 0; i < count; i++)
+            {
+                output = output + "I";
+            }
+            return output;
+        }
+
+        public static string Addition(string num1, string num2)
+        {
+            var translated1 = TranslateInput(num1);
+            var translated2 = TranslateInput(num2);
+
+            var translateOutput = translated1 + translated2;
+
+
+            foreach (var translation in TranslateFromI())
+            {
+                translateOutput = translateOutput.Replace(translation.Key, translation.Value);
+            }
+
+            return translateOutput;
+        }
+
+        public static string TranslateInput(string value)
+        {
+            foreach (var translation in TranslateToI())
+            {
+                value = value.Replace(translation.Key, translation.Value);
+            }
+            return value;
+        }
+    }
+}
